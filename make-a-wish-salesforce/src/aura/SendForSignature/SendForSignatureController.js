3e2cd76850b2bdf3b8aa3fd16603b8a240547d2c
Pramod nagar <pnagar@tractionondemand.com>
2020-10-08 16:16:12 +0530

    Initial commit

--- make-a-wish-salesforce/src/aura/SendForSignature/SendForSignatureController.js
+++ make-a-wish-salesforce/src/aura/SendForSignature/SendForSignatureController.js
@@ -10,6 +10,7 @@
     },
     
     handleOnload : function(component, event, helper) {
+        var contactEmails = [];
         var recUi = event.getParam("recordUi");
         var emailAddress = recUi.record.fields["Sent_to_Email_Address__c"].value;
         component.set("v.wishReqSignEmail",emailAddress);
@@ -18,6 +19,26 @@
             emailAddress = component.get("v.wishReqSignRecord.Wish_Affiliation__r.Contact__r.Email");
         }
         
+        if(recUi.record.fields.Wish_Affiliation__r.value.fields.hasOwnProperty("Contact__r")) {
+            if(!$A.util.isEmpty(recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.npe01__WorkEmail__c.value)) {
+                var workEmail = recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.npe01__WorkEmail__c.value;
+                contactEmails.push({label: workEmail, value: workEmail});
+            }
+            if(!$A.util.isEmpty(recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.npe01__AlternateEmail__c.value)) {
+                var altEmail = recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.npe01__AlternateEmail__c.value;
+                contactEmails.push({label: altEmail, value: altEmail});
+            }
+            if(!$A.util.isEmpty(recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.Alternate_Email_2__c.value)) {
+                var alt2Email = recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.Alternate_Email_2__c.value;
+                contactEmails.push({label: alt2Email, value: alt2Email});
+            }
+            if(!$A.util.isEmpty(recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.RecordTypeId.value)) {
+                var recordTypeId = recUi.record.fields.Wish_Affiliation__r.value.fields.Contact__r.value.fields.RecordTypeId.value;
+                component.set("v.MPRecordTypeId", recordTypeId);
+            }
+        }
+
+        component.set("v.otherEmails", contactEmails);
         component.set("v.emailId", emailAddress);
         if(component.find("sendViaEmail")){
             component.find("sendViaEmail").set("v.value",emailAddress);
@@ -37,7 +58,13 @@
     },
     
     handleSendESign: function (cmp, event, helper) {
-        var emailId = cmp.find("sendViaEmail") ? cmp.find("sendViaEmail").get("v.value") : '';
+        var emailId;
+        
+        if(cmp.find("changeEmail") && cmp.find("changeEmail").get("v.checked")) {
+            emailId = cmp.find("otherEmails") ? cmp.find("otherEmails").get("v.value") : '';
+        } else {
+            emailId = cmp.find("sendViaEmail") ? cmp.find("sendViaEmail").get("v.value") : '';
+        }
         
         if($A.util.isEmpty(emailId) ){
             var toastEvent = $A.get("e.force:showToast");
@@ -75,4 +102,9 @@
         helper.updateEnvelope(cmp, event, actionToPerform);
     },
     
+    enableDisableRadioButtons: function (cmp, event, helper) {
+        var val = event.getSource().get("v.checked");
+    	cmp.find("otherEmails").set("v.disabled", !val);
+        cmp.find("sendViaEmail").set("v.disabled", val);
+	},
 })
