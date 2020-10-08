9c9bc8bb87aa06082866155362295a5b0d03ba09
Gus <gmayer@tractionondemand.com>
2020-10-08 11:44:07 -0700

    Fixing issues

--- make-a-wish-salesforce/src/aura/SendForSignature/SendForSignatureHelper.js
+++ make-a-wish-salesforce/src/aura/SendForSignature/SendForSignatureHelper.js
@@ -1,40 +1,52 @@
 ({
-    getDocgenDetails : function(cmp, event) {
+    initialize: function (cmp) {
+        let action = cmp.get("c.getMetadata");
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === 'SUCCESS') {
+                let data = response.getReturnValue();
+                cmp.set("v.medicalProfessionalRtId", data.medicalProfessionalId);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    getDocgenDetails: function (cmp, event) {
         var emailId;
-        
-        if(cmp.find("changeEmail") && cmp.find("changeEmail").get("v.checked")) {
+
+        if (cmp.find("changeEmail") && cmp.find("changeEmail").get("v.checked")) {
             emailId = cmp.find("otherEmails") ? cmp.find("otherEmails").get("v.value") : '';
         } else {
             emailId = cmp.find("sendViaEmail") ? cmp.find("sendViaEmail").get("v.value") : '';
         }
-        
+
         var action = cmp.get("c.getDocGen");
         action.setParams({
-            wishSignId : cmp.get("v.wishReqSignRecord.Id"),
+            wishSignId: cmp.get("v.wishReqSignRecord.Id"),
         });
-        action.setCallback(this, function(response){
+        action.setCallback(this, function (response) {
             var state = response.getState();
-            if(state === 'SUCCESS'){
-                cmp.set('v.wishForm',response.getReturnValue());
-                
+            if (state === 'SUCCESS') {
+                cmp.set('v.wishForm', response.getReturnValue());
+
                 var actionUpdate = cmp.get("c.updateWishSignature");
                 actionUpdate.setParams({
                     "wrsId": cmp.get("v.WRSId"),
-                    "format" : 'E-Signature',
-                    "emailId" :  emailId
+                    "format": 'E-Signature',
+                    "emailId": emailId
                 });
                 actionUpdate.setCallback(this, function (response) {
                     var state = response.getState();
                     if (state === 'SUCCESS') {
                         //var emailId = cmp.find("sendViaEmail") ? cmp.find("sendViaEmail").get("v.value") : '';
                         //console.log('emailId '+emailId);
-                        
+
                         var attachId = '';
-                        if(cmp.find("fileUpload")){
+                        if (cmp.find("fileUpload")) {
                             attachId = cmp.find("fileUpload").get("v.attachmentId");
                         }
                         var url = window.location.protocol + '//' + window.location.host + '/apex/loop__looplus';
-                        url = url + '?eid=' +  cmp.get("v.wishReqSignRecord.Id");
+                        url = url + '?eid=' + cmp.get("v.wishReqSignRecord.Id");
                         url = url + '&CaseId=' + cmp.get("v.caseId");
                         url = url + '&ddpIds=' + cmp.get("v.wishForm.DocGen_Package_ID__c");
                         url = url + '&deploy=' + cmp.get("v.wishForm.Delivery_Option_ID_eSign__c");
@@ -44,34 +56,31 @@
                         url = url + '&header=false';
                         url = url + '&sidebar=false';
                         url = url + '&autorun=true';
-                        
+
                         if (!cmp.get('v.iframeUrl')) {
                             cmp.set('v.iframeUrl', url);
-                        }
-                        else {
+                        } else {
                             // Set blank and then set in order to reset iframe
                             cmp.set('v.iframeUrl', '');
-                            setTimeout($A.getCallback(function() {
+                            setTimeout($A.getCallback(function () {
                                 cmp.set('v.iframeUrl', url);
                             }), 2000);
-                        }	
-                    }
-                    else if (state === 'ERROR') {
+                        }
+                    } else if (state === 'ERROR') {
                         cmp.set("v.errors", response.getError());
                     }
                 });
                 $A.enqueueAction(actionUpdate);
-            } 
-            else if(state === 'ERROR'){
+            } else if (state === 'ERROR') {
                 cmp.set("v.errors", response.getError());
             }
         });
-        $A.enqueueAction(action);	
+        $A.enqueueAction(action);
     },
-    
-    updateEnvelope: function(cmp,event,actionToPerform) {
-        if(actionToPerform == "Reassign"
-           && ($A.util.isEmpty(cmp.get("v.selectedLookUpRecord")) || ((cmp.find("reassignEmail")) && $A.util.isEmpty( cmp.find("reassignEmail").get("v.value"))))) {
+
+    updateEnvelope: function (cmp, event, actionToPerform) {
+        if (actionToPerform == "Reassign"
+            && ($A.util.isEmpty(cmp.get("v.selectedLookUpRecord")) || ((cmp.find("reassignEmail")) && $A.util.isEmpty(cmp.find("reassignEmail").get("v.value"))))) {
             let toastParams = {
                 title: "Error",
                 message: "Please select a Signer and Send to Email address",
@@ -82,7 +91,7 @@
             toastEvent.fire();
             return;
         }
-        if(actionToPerform == "Reassign"){
+        if (actionToPerform == "Reassign") {
             var validFields = [];
             validFields.push(cmp.find('reassignEmail'));
             var allValid = validFields.reduce(function (validSoFar, inputCmp) {
@@ -99,33 +108,33 @@
                 });
                 toastEvent.fire();
                 return;
-            } 
+            }
         }
         var wishAffList = [];
-        if(actionToPerform == "Reassign"){
-            if(!$A.util.isEmpty(cmp.get("v.selectedLookUpRecord")) && !$A.util.isEmpty(cmp.get("v.selectedLookUpRecord").Wish_Affiliations__r) ){
-                for(let record of cmp.get("v.selectedLookUpRecord").Wish_Affiliations__r){
+        if (actionToPerform == "Reassign") {
+            if (!$A.util.isEmpty(cmp.get("v.selectedLookUpRecord")) && !$A.util.isEmpty(cmp.get("v.selectedLookUpRecord").Wish_Affiliations__r)) {
+                for (let record of cmp.get("v.selectedLookUpRecord").Wish_Affiliations__r) {
                     wishAffList.push(record);
-                } 
+                }
             }
-            
+
         }
-       
+
         var action = cmp.get("c.updateEnvelopeDocusign");
         action.setParams({
-            "action" : actionToPerform,
-            "resendToEmail" : cmp.get("v.wishReqSignEmail"),
-            "wrsId" : cmp.get("v.WRSId"),
-            "voidReason" : cmp.get("v.voidReason"),
-            "selectedCon" : cmp.get("v.selectedLookUpRecord"),
-            "reassignToEmail" : cmp.find("reassignEmail") ? cmp.find("reassignEmail").get("v.value") : "",
-            "wishAffList" : JSON.stringify(wishAffList)
+            "action": actionToPerform,
+            "resendToEmail": cmp.get("v.wishReqSignEmail"),
+            "wrsId": cmp.get("v.WRSId"),
+            "voidReason": cmp.get("v.voidReason"),
+            "selectedCon": cmp.get("v.selectedLookUpRecord"),
+            "reassignToEmail": cmp.find("reassignEmail") ? cmp.find("reassignEmail").get("v.value") : "",
+            "wishAffList": JSON.stringify(wishAffList)
         });
-        action.setCallback(this, function(response){
+        action.setCallback(this, function (response) {
             var state = response.getState();
-            if(state === 'SUCCESS'){
+            if (state === 'SUCCESS') {
                 let res = response.getReturnValue();
-                if(!$A.util.isEmpty(res["errorMessage"])){
+                if (!$A.util.isEmpty(res["errorMessage"])) {
                     var toastEvent = $A.get("e.force:showToast");
                     toastEvent.setParams({
                         "type": "error",
@@ -133,8 +142,7 @@
                         "message": res["errorMessage"]
                     });
                     toastEvent.fire();
-                }
-                else if(!$A.util.isEmpty(res["successMessage"])){
+                } else if (!$A.util.isEmpty(res["successMessage"])) {
                     var toastEvent = $A.get("e.force:showToast");
                     toastEvent.setParams({
                         "type": "success",
@@ -144,13 +152,11 @@
                     toastEvent.fire();
                 }
                 cmp.set("v.refresh", cmp.get("v.refresh") + 1);
-            }
-            else if(state === 'ERROR'){
+            } else if (state === 'ERROR') {
                 cmp.set("v.errors", response.getError());
             }
-            
+
         });
-        $A.enqueueAction(action);	 
-    },
-    
-})
+        $A.enqueueAction(action);
+    }
+});
